1fa11b53e69d29fa722597d93024cf4d6c7aaea8
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-08-23 05:20:28 +0300

    v1.0.0-beta.5

--- resolveModuleLocation.js
+++ resolveModuleLocation.js
@@ -13,25 +13,35 @@ function getFileWithExtensions(resourceName, extensions) {
   }
 }
 
-module.exports = function resolveModuleLocation(moduleName, cwd) {
+function searchScriptInDirectories(moduleName) {
+  return function(dirs) {
+    for (let i = 0; i < dirs.length; i++) {
+      const resourceName = path.join(dirs[i], moduleName);
+      const indexFilename = path.join(resourceName, 'index');
+      const existenFilename = getFileWithExtensions(isDirSync(resourceName) ? indexFilename : resourceName, ['.js', '.es', '.coffee', '.ts']);
+      if (existenFilename) {
+        return existenFilename;
+      }
+    }
+    return Promise.reject();
+  }
+}
+
+module.exports = function resolveModuleLocation(moduleName, cwd, opt) {
   return Promise.resolve()
   .then(function() {
+    if (typeof opt.paths === 'object') {
+      console.log('Search scripts in directories')
+      return searchScriptInDirectories(moduleName)(opt.paths);
+    }
+  })
+  .catch(function() {
     const localFile = path.resolve(cwd, moduleName);
     if (isFileSync(localFile)) {
       resolve(localFile);
     } else {
       return searchLocalErectorDisposition(cwd)
-      .then(function(dirs) {
-        for (let i = 0; i < dirs.length; i++) {
-          const resourceName = path.join(dirs[i], moduleName);
-          const indexFilename = path.join(resourceName, 'index');
-          const existenFilename = getFileWithExtensions(isDirSync(resourceName) ? indexFilename : resourceName, ['.js', '.es', '.coffee', '.ts']);
-          if (existenFilename) {
-            return existenFilename;
-          }
-        }
-        return Promise.reject();
-      })
+      .then(searchScriptInDirectories(moduleName))
       .catch(function() {
         const location = resolvePackage(moduleName);
         if (location instanceof Error) {
